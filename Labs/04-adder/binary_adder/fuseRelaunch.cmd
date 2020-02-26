c14be226c645a2beb457c5d7651680c154f690dd
lab661 <lab661@PC-449.urel.feec.vutbr.cz>
2020-02-26 14:50:04 +0100

    labs4

---
+++ Labs/04-adder/binary_adder/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/libor-matejek/Digital-eletronics1/Labs/04-adder/binary_adder/top_tb00_isim_beh.exe" -prj "/home/lab661/Documents/libor-matejek/Digital-eletronics1/Labs/04-adder/binary_adder/top_tb00_beh.prj" "work.top_tb00" 
