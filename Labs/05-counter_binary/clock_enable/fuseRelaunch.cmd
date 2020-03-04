1d9c1ed1a195f1016df5500f01d5a09386748605
lab661 <lab661@PC-449.urel.feec.vutbr.cz>
2020-03-04 14:48:27 +0100

    Labs4

---
+++ Labs/05-counter_binary/clock_enable/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/libor-matejek/Digital-eletronics1/Labs/05-counter_binary/clock_enable/test2_isim_beh.exe" -prj "/home/lab661/Documents/libor-matejek/Digital-eletronics1/Labs/05-counter_binary/clock_enable/test2_beh.prj" "work.test2" 
