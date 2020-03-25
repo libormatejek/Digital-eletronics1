dc76f6993f9161e8f18be1357b001c301c79c82c
libormatejek <60597307+libormatejek@users.noreply.github.com>
2020-03-25 18:31:56 +0100

    Add files via upload

---
+++ Labs/08-trafficlights/trafficlights/test.vhd
@@ -0,0 +1,97 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   13:04:26 03/23/2020
+-- Design Name:   
+-- Module Name:   D:/ise/Labs/07-clockwatch/trafficlights/test.vhd
+-- Project Name:  trafficlights
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: trafficlights
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY test IS
+END test;
+ 
+ARCHITECTURE behavior OF test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT trafficlights
+    PORT(
+         clk_i : IN  std_logic;
+         srst_n_i : IN  std_logic;
+         cnt_en_i : IN  std_logic;
+         lights : OUT  std_logic_vector(5 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk_i : std_logic := '0';
+   signal srst_n_i : std_logic := '0';
+   signal cnt_en_i : std_logic := '0';
+
+ 	--Outputs
+   signal lights : std_logic_vector(5 downto 0);
+
+   -- Clock period definitions
+   constant clk_i_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: trafficlights PORT MAP (
+          clk_i => clk_i,
+          srst_n_i => srst_n_i,
+          cnt_en_i => cnt_en_i,
+          lights => lights
+        );
+
+   -- Clock process definitions
+   clk_i_process :process
+   begin
+		clk_i <= '0';
+		wait for clk_i_period/2;
+		clk_i <= '1';
+		wait for clk_i_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for clk_i_period*10;
+
+      -- insert stimulus here 
+		srst_n_i <= '1';
+		cnt_en_i <= '1';
+      wait;
+   end process;
+
+END;
---
+++ Labs/08-trafficlights/trafficlights/trafficlights.vhd
@@ -0,0 +1,84 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    17:10:51 03/18/2020 
+-- Design Name: 
+-- Module Name:    trafficlights - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+--use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity trafficlights is
+port (
+	clk_i    	: in  std_logic;
+	srst_n_i 	: in  std_logic;   -- Synchronous reset (active low)
+	cnt_en_i 	: in  std_logic;	 -- stopwatch enable
+	
+	lights	: out std_logic_vector(5 downto 0) --(stav 1)
+	);
+end trafficlights;
+
+
+architecture Behavioral of trafficlights is
+signal s_en  : std_logic;
+signal s_d1 : std_logic_vector(3 downto 0) := x"0";
+signal s_1 : std_logic_vector(5 downto 0) := "000000";
+begin
+     
+
+
+CL_ENABLE: entity work.clock_enable
+        generic map (g_NPERIOD=>x"0004")
+        port map (
+                  clk_i=>clk_i,
+                  srst_n_i=>srst_n_i,
+                  clock_enable_o=>s_en
+                 );
+					  
+					  
+	p1 : process(clk_i, srst_n_i, cnt_en_i)
+	--variable s_d1 : std_logic_vector(3 downto 0) := x"0";
+  -- variable s_1 : std_logic_vector(5 downto 0) := "000000";	
+ 
+				
+	begin
+		
+		   if s_en  = '1' and cnt_en_i = '1' then
+			s_d1 <= s_d1 + x"1";
+			if s_d1 < x"6" then s_1 <= "100001"; end if; 
+			if (s_d1 = x"7") then s_1 <= "100010"; end if;
+			if (s_d1 = x"8") then s_1 <= "100100"; end if; 			
+			if (s_d1 > x"8" and s_d1 < x"d") then s_1 <= "001100"; end if;
+			if (s_d1 = x"e") then s_1 <= "010100"; end if;
+			if (s_d1 = x"f") then s_1 <= "100001";  end if;			
+				
+		end if;
+		lights <= s_1;
+		
+		
+	end process;
+end Behavioral;
+
